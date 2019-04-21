237d5794bc9cc0f6d885c2b52adbff991bee9faa
mmio <dominikvasko96@gmail.com>
2019-04-21 16:53:26 +0200

    Adding new stuff

--- VAsVAFrontEnd/screens/BoulderProblemScreen.js
+++ VAsVAFrontEnd/screens/BoulderProblemScreen.js
@@ -12,7 +12,7 @@ import {
   Text,
   Thumbnail
 } from "native-base";
-import { ImageBackground, View } from "react-native";
+import { ImageBackground, View, FlatList} from "react-native";
 import AppHeader from "../components/AppHeader.js";
 import { Col, Row, Grid } from "react-native-easy-grid";
 import { createIconSetFromFontello } from "react-native-vector-icons";
@@ -39,15 +39,16 @@ export default class BoulderProblemScreen extends React.Component {
 
     componentDidMount() {
         fetch('http://192.168.2.9:8080/Insult')
-                .then(result=> {
-                    //this.setState({problems: result.entity._embedded.problems});
-                    console.log(result);
+                .then((response) =>
+                    response.json()
+                )
+                .then((problems) => {
+                    this.setState({problems})
                 })
                 .catch(err => {
                     console.log(err);
                 });
 	}
-
 
   closeDrawer() {
     this.drawer._root.close();
@@ -58,174 +59,23 @@ export default class BoulderProblemScreen extends React.Component {
   }
 
   render() {
+    const problems = this.state.problems;
+
+    const boulderProblemList = problems
+    .filter(problem => problem.type === "boulder")
+    .map(problem =>
+        <View>
+            <Text>{problem.name}</Text>
+        </View>
+    );
+
     return (
-      <StyleProvider style={getTheme(material)}>
-        <Drawer
-          ref={ref => {
-            this.drawer = ref;
-          }}
-          content={
-            <SideBar
-              navigation={this.props.navigation}
-              closeDrawer={() => this.props.closeDrawer()}
-            />
-          }
-          onClose={() => this.closeDrawer()}
-        >
-          <Container>
-            <AppHeader
-              openDrawer={() => this.openDrawer()}
-              navigation={this.props.navigation}
-            />
-            <Content
-              contentContainerStyle={{ flex: 1, heigth: "100%", width: "100%" }}
-            >
-              <ImageBackground
-                source={require("../img/backgroundImg.jpg")}
-                style={{
-                  flex: 1,
-                  width: "100%",
-                  height: "100%",
-                  resizeMode: "stretch"
-                }}
-                blurRadius={0.7}
-              >
-               <TintedOpacity/>
-                <Grid style={{ margin: "2%" }}>
-                  <Row size={11} style={{ marginVertical: "1%" }}>
-                    <Col style={{ marginRight: "2%" }}>
-                      <Button
-                        dark
-                        style={{
-                          flex: 1,
-                          flexDirection: "column",
-                          alignItems: "center",
-                          justifyContent: "center",
-                          width: "100%",
-                          height: "100%"
-                        }}
-                        onPress={() => this.props.navigation.navigate("Profile")}
-                      >
-                        <Thumbnail
-                          extra-large
-                          source={require("../img/Palino.jpg")}
-                          style={{ margin: "10%" }}
-                        />
-                        <Text>Moj profil {this.props.problems} </Text>
-                      </Button>
-                    </Col>
-                    <Col style={{ marginLeft: "2%" }}>
-                      <Row style={{ marginBotom: "3%" }}>
-                        <Button
-                          style={{
-                            flex: 1,
-                            flexDirection: "column",
-                            alignItems: "center",
-                            justifyContent: "center",
-                            width: "100%",
-                            height: "100%",
-                            backgroundColor: "#f1c40f"
-                          }}
-                        >
-                          <Text
-                            style={{
-                              textAlign: "center",
-                              fontSize: 16,
-                              color: "#232B2B"
-                            }}
-                          >
-                            BOULDER PROBLÉMY
-                          </Text>
-                          <Icon
-                            type="FontAwesome5"
-                            name="hand-rock"
-                            style={{ fontSize: 50, color: "#232B2B" }}
-                          />
-                        </Button>
-                      </Row>
-                      <Row style={{ marginTop: "3%" }}>
-                        <Button
-                          style={{
-                            flex: 1,
-                            flexDirection: "column",
-                            justifyContent: "center",
-                            width: "100%",
-                            height: "100%",
-                            backgroundColor: "#2980b9"
-                          }}
-                        >
-                          <CustomIcon name="climber" size={60} color="white" />
-                          <Text style={{ textAlign: "center", fontSize: 16 }}>
-                            PROBLÉMY NA STENE
-                          </Text>
-                        </Button>
-                      </Row>
-                    </Col>
-                  </Row>
-                  <Row size={5} style={{ marginVertical: "1%" }}>
-                    <Col size={2} style={{ marginRight: "2%" }}>
-                      <Button
-                        style={{
-                          flex: 1,
-                          flexDirection: "column",
-                          alignItems: "center",
-                          justifyContent: "center",
-                          width: "100%",
-                          height: "100%",
-                          backgroundColor: "#2980b9"
-                        }}
-                      >
-                        <Text style={{ fontSize: 20 }}>MAPA</Text>
-                        <Icon
-                          type="FontAwesome5"
-                          name="map-marked-alt"
-                          style={{ fontSize: 45 }}
-                        />
-                      </Button>
-                    </Col>
-                    <Col size={3} style={{ marginLeft: "2%" }}>
-                      <Button
-                        style={{
-                          flex: 1,
-                          flexDirection: "column",
-                          alignItems: "center",
-                          justifyContent: "center",
-                          width: "100%",
-                          height: "100%",
-                          backgroundColor: "#d35400"
-                        }}
-                      >
-                        <Icon
-                          type="FontAwesome"
-                          name="qrcode"
-                          style={{ fontSize: 60, color: "#232B2B" }}
-                        />
-                        <Text style={{ color: "#232B2B" }}>SKENUJ QR KÓD</Text>
-                      </Button>
-                    </Col>
-                  </Row>
-                  <Row size={4} style={{ marginVertical: "1%" }}>
-                    <Button
-                      dark
-                      style={{
-                        flex: 1,
-                        flexDirection: "row",
-                        alignItems: "center",
-                        justifyContent: "center",
-                        width: "100%",
-                        height: "100%"
-                      }}
-                    >
-                      <Icon type="FontAwesome5" name="bars" />
-                      <Text style={{ fontSize: 26 }}>REBRÍČEK</Text>
-                    </Button>
-                  </Row>
-                </Grid>
-              </ImageBackground>
-            </Content>
-          </Container>
-        </Drawer>
-      </StyleProvider>
+        <View style={styles.container}>
+          <View style={styles.footer}>
+            <View><Text>Boulder problems</Text></View>
+            {boulderProblemList}
+          </View>
+        </View>
     );
   }
 }
